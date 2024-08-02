d4ec9ec21fb827965550972bf0b0d726911afa00
Jagadeesh <jagadeeshperiketi2369@gmail.com>
2024-08-01 23:07:09 -0500

    Linear search

---
+++ src/Demo.java
@@ -0,0 +1,28 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        int nums[] = {2, 4 , 6, 8, 10 };
+        int target = 11;
+        int result = linearSearch(nums, target);
+
+        if(result != -2)
+
+        System.out.println("Element found at Index : " + result);
+
+        else
+            System.out.println("Element not found");
+
+
+
+    }
+
+    public static int linearSearch(int[] nums, int target) {
+
+        for (int i=0;i<nums.length; i++){
+            if(nums[i] == target)
+                return i;
+        }
+        return -2;
+
+    }
+}
